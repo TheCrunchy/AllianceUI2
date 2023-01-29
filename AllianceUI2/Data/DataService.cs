eacd96b871383c1e0e406dfc6d56450880aef51e
TheCrunchy <crunchycamsta@gmail.com>
2023-01-29 21:22:22 +0000

    Initial commit

---
+++ AllianceUI2/Data/DataService.cs
@@ -0,0 +1,56 @@
+﻿using AlliancesPlugin;
+using AlliancesPlugin.Alliances;
+
+namespace AllianceUI2.Data
+{
+    public class DataService
+    {
+        public Alliance GetData(Guid id)
+        {
+            return new Alliance()
+            {
+                name = "This is test data",
+                CustomRankPermissions = new Dictionary<string, RankPermissions>()
+                {
+                    {
+                       "test",
+                       new RankPermissions()
+                        {
+                            RankName = "test",
+                            taxRate = 5,
+                            permissionLevel = 100
+                        }
+                    }
+                    ,
+                    {  "test2",
+                      new RankPermissions()
+                        {
+                            RankName = "test2",
+                            taxRate = 3,
+                            permissionLevel = 101
+                        }
+                    }
+                    ,
+                    {  "test3",
+                      new RankPermissions()
+                        {
+                            RankName = "Alan",
+                            taxRate = 100,
+                            permissionLevel = 99
+                        }
+                    }
+                    ,
+                    {  "test4",
+                      new RankPermissions()
+                        {
+                            RankName = "Dave",
+                            taxRate = 1,
+                            permissionLevel = 45
+                        }
+                    }
+                }
+            };
+        }
+    }
+}
+
